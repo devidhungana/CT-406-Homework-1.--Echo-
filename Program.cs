a8ac3f6bdcd0d5fce67e3c416d1962a27173dd6b
devidhungana <43016940+devidhungana@users.noreply.github.com>
2018-09-30 23:29:45 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,31 +4,21 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Echo
+namespace Enviroment
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseIISIntegration()
-                .Configure(app =>
-                {
-                    app.Run(async (context) =>
-                    {
-                        var path = context.Request.Path;
-                        await context.Response.WriteAsync(path);
-                    });
-                })
-                .Build();
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
